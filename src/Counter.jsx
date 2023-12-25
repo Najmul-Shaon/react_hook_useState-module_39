5651562c18249036cf8557ee46506860ff4f321b
Najmul shaon <najmul.nh.shaon@gmail.com>
2023-12-25 16:43:23 +0600

    decrease and negative validation added

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -6,10 +6,20 @@ export default function Counter() {
     const newCount = count + 1;
     setCount(newCount);
   };
+
+  const handleDecrease = () => {
+    const newCount = count - 1;
+    if (newCount >= 0) {
+      setCount(newCount);
+    } else {
+      alert("NaN");
+    }
+  };
   return (
     <div>
       <h3>Counter: {count} </h3>
       <button onClick={handleIncrease}>Add</button>
+      <button onClick={handleDecrease}>Decrease</button>
     </div>
   );
 }
